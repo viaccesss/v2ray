c1ae10ea9cce7b5e3a30b5e598a1aba32bf65b6f
viaccesss <62853867+viaccesss@users.noreply.github.com>
2022-01-02 22:19:48 +0800

    Add files via upload

--- 233v334.sh
+++ 233v334.sh
@@ -8,7 +8,7 @@ else
 fi
 
 rm -rf /tmp/233boy/v2ray
-git clone https://github.com/crazypeace/v2ray -b "master" /tmp/233boy/v2ray --depth=1
+git clone https://github.com/viaccesss/v2ray -b "main" /tmp/233boy/v2ray --depth=1
 cd /tmp/233boy/v2ray
 chmod +x install.sh
 ./install.sh local
